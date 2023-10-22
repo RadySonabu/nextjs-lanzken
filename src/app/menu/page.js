5bca49b3e83e842db6cac4bb928618f80747d52d
Lanzken <lancekennethn@gmail.com>
2023-10-22 22:11:44 +0800

    images

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -3,128 +3,209 @@ import MyNavbar from '@/components/index/Navbar'
 import Image from 'next/image'
 import React from 'react'
 
-const products = [
-    {
-      id: 1,
-      name: 'Chicken Inasal',
-      href: '#',
-      imageSrc: 'https://media.istockphoto.com/id/1316967319/photo/chicken-inasal-a-variant-of-lechon-manok-on-a-banana-leaf-on-top-of-wooden-platter-it-is.jpg?s=612x612&w=0&k=20&c=a3BxaJuUdEfd4gG2k5_w-iDtuf_pbHVgvma8EkE-nVQ=',
-      imageAlt: "",
-      price: 'P349',
-      
-    },
-    
-    {
-        id: 2,
-        name: 'Fried Legpops',
-        href: '#',
-        imageSrc: 'https://media.istockphoto.com/id/1056651714/photo/fried-chicken-wings-and-chicken-legs.jpg?s=612x612&w=0&k=20&c=7XWanAjRFSePK0VXHG5WwNg0SfUEWJTddgL3XKnV1KM=',
-        imageAlt: "",
-        price: 'P299',
-        
-      },
-
-      {
-        id: 3,
-        name: 'Chicken Barbecubes',
-        href: '#',
-        imageSrc: 'https://media.istockphoto.com/id/1142603303/photo/grilled-meat-and-vegetables-on-skewers.jpg?s=612x612&w=0&k=20&c=IsvHBVukF10ZLODFrM0OiqkyDgd35Dd1L6oq3fwOE4I=',
-        imageAlt: "",
-        price: 'P49',
-        
-      },
-
-      {
-        id: 4,
-        name: 'Grilled Chick Breast',
-        href: '#',
-        imageSrc: 'https://media.istockphoto.com/id/580114116/photo/grilled-chicken-fillets-in-a-spicy-marinade.jpg?s=612x612&w=0&k=20&c=ZpRLP1xUUtAoiXPSGxPh9HUJ9L5JVh9wIfd-cRYTj1k=',
-        imageAlt: "",
-        price: 'P499',
-        
-      },
-
-      {
-        id: 5,
-        name: 'Chick in Nuggets',
-        href: '#',
-        imageSrc: 'https://media.istockphoto.com/id/700771078/photo/fried-crispy-chicken-nuggets-with-ketchup-on-white-board.jpg?s=612x612&w=0&k=20&c=3VTbi_VYMo6KBKXuMacXgM0Z1r9yinWb97qLeuE5iVw=',
-        imageAlt: "",
-        price: 'P199',
-        
-      },
-
-      {
-        id: 6,
-        name: 'Chick in Burger',
-        href: '#',
-        imageSrc: 'https://media.istockphoto.com/id/1202811884/photo/vegan-meatless-plant-based-protein-chicken-strip-burger-on-a-whole-wheat-bun-with-lettuce.jpg?s=612x612&w=0&k=20&c=lKhSG7nAzsSOTTQLTBg2tTtuJh0RGGt6yy5P9LANJag=',
-        imageAlt: "",
-        price: 'P99',
-        
-      },
-
-      {
-        id: 7,
-        name: 'Chicken Wings',
-        href: '#',
-        imageSrc: 'https://media.istockphoto.com/id/835903320/photo/baked-chicken-wings-with-sesame-seeds-and-sweet-chili-sauce-on-white-wooden-board.jpg?s=612x612&w=0&k=20&c=SH8ZCkEKuWD_wxulpntIJ0uD4yRnUf9UXovQwSwrmmA=',
-        imageAlt: "",
-        price: 'P259',
-        
-      },
-
-      {
-        id: 8,
-        name: 'Chick in Rice',
-        href: '#',
-        imageSrc: 'https://media.istockphoto.com/id/672810138/photo/a-traditional-asian-dish-pilaf-with-meat-mushrooms-and-pepper-capi-in-a-bowl-on-a-dark-wooden.jpg?s=612x612&w=0&k=20&c=aDB8wTyGNycfOv0h3VQHj8GaMprTvDWcXdn4zRvumUg=',
-        imageAlt: "",
-        price: 'P79',
-        
-      },
-  ]
-
-function Menu() {
+function Deals() {
   return (
     <div>
         <MyNavbar/>
+  
+  <div class="min-h-screen bg-gray-100 flex flex-col justify-center">
+  <div class="relative m-3 flex flex-wrap mx-auto justify-center">
+    
 
-        <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">WHAT&apos;S HOT</h2>
-
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                </div>
-                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-              </div>
-            </div>
-          ))}
-
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200"  class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1232401725/photo/table-scene-of-assorted-take-out-or-delivery-foods-top-down-view-on-a-dark-wood-banner.jpg?s=612x612&w=0&k=20&c=kATZRUmpgZHYxOAC7nVHPm5HRDr_BCfaLMr5nko_0Eg="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick All In</p>
+          <p class="text-md text-gray-800 mt-0">P1099</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
         </div>
       </div>
     </div>
 
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1455378782/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=g3sfhyo8wxLjAyg7WJG0OyJjni4hCbKJyUhzjSMk_E0="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Overload</p>
+          <p class="text-md text-gray-800 mt-0">P1399</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/841164248/photo/wooden-board-on-a-table-with-assorted-fried-appetizers.jpg?s=612x612&w=0&k=20&c=Ka4cD4SEV9hNN4oCyM7XRgb5miDL3IMKVaRJ2JhhO14="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Bundle</p>
+          <p class="text-md text-gray-800 mt-0">P899</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1455378782/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=g3sfhyo8wxLjAyg7WJG0OyJjni4hCbKJyUhzjSMk_E0="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Overload</p>
+          <p class="text-md text-gray-800 mt-0">P1399</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1455378782/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=g3sfhyo8wxLjAyg7WJG0OyJjni4hCbKJyUhzjSMk_E0="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Overload</p>
+          <p class="text-md text-gray-800 mt-0">P1399</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1455378782/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=g3sfhyo8wxLjAyg7WJG0OyJjni4hCbKJyUhzjSMk_E0="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Overload</p>
+          <p class="text-md text-gray-800 mt-0">P1399</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1455378782/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=g3sfhyo8wxLjAyg7WJG0OyJjni4hCbKJyUhzjSMk_E0="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Overload</p>
+          <p class="text-md text-gray-800 mt-0">P1399</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1455378782/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=g3sfhyo8wxLjAyg7WJG0OyJjni4hCbKJyUhzjSMk_E0="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Overload</p>
+          <p class="text-md text-gray-800 mt-0">P1399</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div class="overflow-x-hidden rounded-2xl relative">
+        <Image width="200" height="200" class="h-40 rounded-2xl w-full object-cover" src="https://media.istockphoto.com/id/1455378782/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=g3sfhyo8wxLjAyg7WJG0OyJjni4hCbKJyUhzjSMk_E0="/>
+        <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </p>
+      </div>
+      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+        <div>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Chick in Overload</p>
+          <p class="text-md text-gray-800 mt-0">P1399</p>
+        </div>
+        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+        <a href="#"
+						class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Buy Now</a>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
         <Footer/>
     </div>
   )
 }
 
-export default Menu
+export default Deals
